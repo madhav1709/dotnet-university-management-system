7ad06487e871fde2848fa150554447bd3b1e77ee
Thomas Clark <tnc1997@virginmedia.com>
2019-06-13 14:02:40 +0100

    build: update dependencies

--- UniversityManagementSystem.Apps.Blazor.Services/AuthenticationService.cs
+++ UniversityManagementSystem.Apps.Blazor.Services/AuthenticationService.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using UniversityManagementSystem.Apps.Blazor.Models;
+using static Microsoft.JSInterop.DotNetObjectRef;
 
 namespace UniversityManagementSystem.Apps.Blazor.Services
 {
@@ -19,7 +20,7 @@ namespace UniversityManagementSystem.Apps.Blazor.Services
         {
             TaskCompletionSource = new TaskCompletionSource<User>();
 
-            await JsRuntime.InvokeAsync<object>("user", new DotNetObjectRef(this));
+            await JsRuntime.InvokeAsync<object>("user", Create(this));
 
             var user = await TaskCompletionSource.Task;
 
@@ -28,12 +29,12 @@ namespace UniversityManagementSystem.Apps.Blazor.Services
 
         public async Task LoginAsync()
         {
-            await JsRuntime.InvokeAsync<object>("login", new DotNetObjectRef(this));
+            await JsRuntime.InvokeAsync<object>("login", Create(this));
         }
 
         public async Task LogoutAsync()
         {
-            await JsRuntime.InvokeAsync<object>("logout", new DotNetObjectRef(this));
+            await JsRuntime.InvokeAsync<object>("logout", Create(this));
         }
 
         [JSInvokable]
